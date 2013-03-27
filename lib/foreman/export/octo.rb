188b28a51ccf284eefa8c6fdf09063d874ea5f4a
Georg Kunz <kwd@gmx.ch>
2013-03-27 21:27:41 +0100

    Add octo specific exporter to generate run script

---
+++ lib/foreman/export/octo.rb
@@ -0,0 +1,18 @@
+require "erb"
+require "foreman/export"
+
+class Foreman::Export::Upstart < Foreman::Export::Base
+
+  def export
+    super
+
+    engine.each_process do |name, process|
+      next if engine.formation[name] < 1
+      next if ['rake', 'console'].include?(name)
+
+      port = engine.port_for(process, 1)
+      write_template "octo/run.erb", "run-#{app}-#{name}", binding
+      chmod 0755, "run-#{app}-#{name}"
+    end
+  end
+end
